57f17cf2caedd4c32328873ed89e14b7f1574121
Lucas <lucas.werner92@gmail.com>
2024-12-19 19:09:11 -0300

    Initial commit, project creation

---
+++ src/main/java/com/devsuperior/componenteseinjecao/ComponentesEInjecaoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.componenteseinjecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentesEInjecaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentesEInjecaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsuperior/componenteseinjecao/ComponentesEInjecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.componenteseinjecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentesEInjecaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
